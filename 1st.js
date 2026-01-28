3b6993121d2d27fb1fe552fd7a25f74242dcfa68
Raunak00-1 <kasaudhanraunak0001@gmail.com>
2026-01-29 00:26:44 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,19 @@
+// let day = 6;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid Day");
+// }
+
+let result=18
+if(age==18)
+confirm("Are you sure")
